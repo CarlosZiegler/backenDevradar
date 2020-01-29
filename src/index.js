fa1b9d694eabc648179882e693db24021ed8da83
CarlosZiegler <carlos.ziegler@gmail.com>
2020-01-29 11:06:13 +0100

    add console.log port

--- src/index.js
+++ src/index.js
@@ -20,4 +20,7 @@ app.use(express.json())
 
 app.use(routes)
 
-server.listen(process.env.PORT || 3000)
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => console.log(`Server is listening on port ${PORT}...`));
+
